fd5b9e303684f9131512a3aebf5db1e5cf3c63f4
sbaranchuk <sbaranchuk@protonmail.com>
2022-11-28 12:02:31 +0300

    Added project to repository + method "someMethod" in ViewController.

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Admin on 28/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func someMethod() {
+
+    }
+
+}
+
